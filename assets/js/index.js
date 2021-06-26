77b3db3407a04316cc1483ce932b4e6073a87e90
Paulo Almeida <paulocomy1408@gmail.com>
2021-06-25 21:10:10 -0400

    terminando navegação lateral

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,13 @@ const bgNav = document.querySelector(".background-nav")
 
 btn.addEventListener("click", () => {
     sideNav.classList.toggle("is-active")
+    bgNav.classList.toggle("bg-active")
 })
 
 document.addEventListener("click", (evento) => {
     if (evento.target == bgNav) {
         sideNav.classList.remove("is-active")
+        bgNav.classList.remove("bg-active")
     }
 })
+
