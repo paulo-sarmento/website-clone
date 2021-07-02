228884223ea2c11e2c764921234aded134ec9cc5
Paulo Almeida <paulocomy1408@gmail.com>
2021-07-01 22:20:03 -0400

    finalizado dropdown e animações do menu lateral. falta a animação do ícone

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector(".humburger-btn")
 const sideNav = document.querySelector(".side-nav-container")
 const bgNav = document.querySelector(".background-nav")
+const sideLinkGames = document.querySelector("[data-link-games]")
+const sideLinkHardware = document.querySelector("[data-link-hardware]")
 
 btn.addEventListener("click", () => {
     sideNav.classList.toggle("is-active")
@@ -14,3 +16,38 @@ document.addEventListener("click", (evento) => {
     }
 })
 
+sideLinkGames.addEventListener("click", () => {
+    const icon = document.querySelector("[data-section-games]")
+    const navExpandido = document.querySelector(".side-nav-expandido-games")
+    const containerNavExpandido = document.querySelector("#navExpandidoGames")
+
+    icon.classList.toggle("open-icon")
+
+    if (icon.classList.contains('open-icon')) {
+        navExpandido.classList.add("showNav")
+        containerNavExpandido.style.display = 'flex'
+        sideLinkGames.classList.add("showNavExpandidoGames")
+    } else {
+        navExpandido.classList.remove("showNav")
+        containerNavExpandido.style.display = 'none'
+        sideLinkGames.classList.remove("showNavExpandidoGames")
+    }
+})
+
+sideLinkHardware.addEventListener("click", () => {
+    const icon = document.querySelector("[data-section-hardware]")
+    const NavExpandido = document.querySelector(".side-nav-expandido-hardware")
+    const containerNavExpandido = document.querySelector("#navExpandidoHardware")
+
+    icon.classList.toggle("open-icon")
+
+    if (icon.classList.contains('open-icon')) {
+        NavExpandido.classList.add("showNav")
+        containerNavExpandido.style.display = 'flex'
+        sideLinkHardware.classList.add("showNavExpandidoHardware")
+    } else {
+        NavExpandido.classList.remove("showNav")
+        containerNavExpandido.style.display = 'none'
+        sideLinkHardware.classList.remove("showNavExpandidoHardware")
+    }
+})
